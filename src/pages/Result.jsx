09a7d0ba7c8ada4dcdecb2ce195ae1f4b6aca613
Виталий Лобанов <vitalijlobanov@MacBook-Pro-Vitalij.local>
2026-09-14 23:07:39 +0300

    исправлен импорт useParams на странице результата

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import { supabase } from '../lib/supabase'
 import './Result.css'
 
